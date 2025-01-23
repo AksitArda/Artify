dd3d7017918d15a4d60ad8d392b12fcb1a4789bd
AksitArda <aksithuseyinarda@gmail.com>
2025-01-24 00:50:52 +0300

    getImages get request

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'getImages.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<List<Images>> futureImages;
+
+  @override
+  void initState() {
+    super.initState();
+    futureImages = fetchImages();
+  }
+
   final List<String> popularImages = [
     'https://pbs.twimg.com/media/FwbKDFfWIAE4dl3?format=jpg&name=large',
     'https://images.unsplash.com/photo-1731432247084-926d73272e63?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -8,15 +22,6 @@ class Home extends StatelessWidget {
     'https://images.unsplash.com/photo-1711560217827-dbcb4de4e35d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     'https://images.pexels.com/photos/775203/pexels-photo-775203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  ];
-
-  final List<String> newImages = [
-    'https://images.unsplash.com/photo-1528067753492-69dda1cd56df?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1711560217827-dbcb4de4e35d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1648878136531-15e7d3a88e76?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1523524709877-a25a07f84b6a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1516310029508-8f346b013f45?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1571513151379-9612cf354937?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   ];
 
   @override
@@ -28,13 +33,23 @@ class Home extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Popular Section
-            Row(children: [Text(
-              'Popular',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),SizedBox(width: 4,),Icon(Icons.star, color:Colors.deepPurple,),],),
+            Row(
+              children: [
+                Text(
+                  'Popular',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Icon(
+                  Icons.star,
+                  color: Colors.deepPurple,
+                ),
+              ],
+            ),
             SizedBox(height: 10),
             SizedBox(
               height: 300,
@@ -51,8 +66,8 @@ class Home extends StatelessWidget {
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),
                       child: FadeInImage.assetNetwork(
-                        placeholder: 'assets/placeholder.png', // Yüklenme sırasında gösterilecek resim
-                        image: popularImages[index], // Resim URL'si
+                        placeholder: 'assets/placeholder.png',
+                        image: popularImages[index],
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -62,39 +77,67 @@ class Home extends StatelessWidget {
             ),
             SizedBox(height: 20),
             // New Section
-            Row(children: [Text(
-              'New',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),SizedBox(width: 4,),Icon(Icons.photo, color:Colors.deepPurple,),],),
+            Row(
+              children: [
+                Text(
+                  'New',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Icon(
+                  Icons.photo,
+                  color: Colors.deepPurple,
+                ),
+              ],
+            ),
             SizedBox(height: 10),
-            GridView.builder( //newImages listesinde ki resim url'lerini gird e ekler
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: newImages.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.only(right: 10),
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: FadeInImage.assetNetwork(
-                      placeholder: 'assets/placeholder.png', // Yüklenme sırasında gösterilecek resim
-                      image: newImages[index], // Resim URL'si
-                      fit: BoxFit.cover,
+            FutureBuilder<List<Images>>(
+              future: futureImages,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Text(
+                    'Error: ${snapshot.error}',
+                    style: TextStyle(color: Colors.red),
+                  );
+                } else if (snapshot.hasData) {
+                  final newImages = snapshot.data!.map((image) => image.imageSlug).toList();
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: newImages.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
                     ),
-                  ),
-                );
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: FadeInImage.assetNetwork(
+                            placeholder: 'assets/placeholder.png',
+                            image: "http://192.168.1.80:3000/" + newImages[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Text(
+                    'No data available',
+                    style: TextStyle(color: Colors.white),
+                  );
+                }
               },
             ),
           ],
