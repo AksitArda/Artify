27a474ca2c58712030a341404b6334118efdd25a
AksitArda <aksithuseyinarda@gmail.com>
2025-01-24 02:32:23 +0300

    Upload image sketch

--- app/lib/search.dart
+++ app/lib/search.dart
@@ -1,60 +1,117 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
-class Search extends StatelessWidget {
-  const Search({super.key});
+class Search extends StatefulWidget {
+  @override
+  _ImageUploadPageState createState() => _ImageUploadPageState();
+}
+
+class _ImageUploadPageState extends State<Search> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _imageTitleController = TextEditingController();
+  final TextEditingController _imageDescController = TextEditingController();
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      }
+    });
+  }
+
+  Future<void> _uploadImage() async {
+    if (_image == null) return;
+
+    final uri = Uri.parse("http://192.168.1.80:3000/uploadImage");
+    var request = http.MultipartRequest('POST', uri);
+
+    request.fields['userName'] = _usernameController.text;
+    request.fields['imageTitle'] = _imageTitleController.text;
+    request.fields['imageDesc'] = _imageDescController.text;
+
+    var file = await http.MultipartFile.fromPath('file', _image!.path);
+    request.files.add(file);
+    
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print("Resim başarıyla yüklendi!");
+    } else {
+      print("Resim yüklenirken bir hata oluştu.");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> history = ['Red Car', 'Space', 'Galaxy', 'Phone', 'Test','Keyvan Arasteh'];
-
-    return Padding(
-      padding: EdgeInsets.only(left: 20, right: 20),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            style: TextStyle(color: Colors.white),
-            decoration: new InputDecoration(
-              labelText: "Search",
-              fillColor: Colors.white,
-              border: new OutlineInputBorder(
-                borderRadius: new BorderRadius.circular(15.0),
-                borderSide: new BorderSide(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resim Yükleme Sayfası"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Kullanıcı Adı'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Lütfen bir kullanıcı adı girin';
+                  }
+                  return null;
+                },
               ),
-              //fillColor: Colors.green
-            ),
-          ),
-          SizedBox(height: 15,),
-          ListView.builder(
-            itemCount: history.length,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              return Container(
-                child: Padding(padding: EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment
-                        .spaceBetween, // Öğeler arasına boşluk koyar
-                    children: [
-                      Text(
-                        history[index].toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                      Icon(
-                        Icons.close,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
+              TextFormField(
+                controller: _imageTitleController,
+                decoration: InputDecoration(labelText: 'Resim Başlığı'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Lütfen bir başlık girin';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _imageDescController,
+                decoration: InputDecoration(labelText: 'Resim Açıklaması'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Lütfen bir açıklama girin';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _pickImage,
+                child: Text('Resim Seç'),
+              ),
+              if (_image != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Image.file(_image!),
                 ),
-              );
-            },
-          )
-        ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _uploadImage();
+                  }
+                },
+                child: Text('Resmi Yükle'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
