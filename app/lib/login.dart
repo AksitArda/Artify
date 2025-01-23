a47f6587c791ae15c219310c0d939f9d8b496774
Efe <efecakmakci34@gmail.com>
2025-01-23 18:19:43 +0300

    Update login.dart

--- app/lib/login.dart
+++ app/lib/login.dart
@@ -8,10 +8,11 @@ class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
-  State<Login> createState() => _SplashScreenState();
+  State<Login> createState() => _LoginState();
 }
 
-class _SplashScreenState extends State<Login> with SingleTickerProviderStateMixin {
+class _LoginState extends State<Login> {
+  bool _isPasswordVisible = false; // Şifrenin görünürlük durumu
 
   @override
   void initState() {
@@ -19,7 +20,8 @@ class _SplashScreenState extends State<Login> with SingleTickerProviderStateMixi
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   }
 
-  void disponse() {
+  @override
+  void dispose() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
     super.dispose();
   }
@@ -27,179 +29,195 @@ class _SplashScreenState extends State<Login> with SingleTickerProviderStateMixi
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 36, 36, 36),
-      body:  Container(
+      backgroundColor: const Color.fromARGB(255, 36, 36, 36),
+      body: Container(
         width: double.infinity,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
               Color.fromARGB(255, 36, 36, 36),
               Colors.deepPurpleAccent,
-            ])),
+            ],
+          ),
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
+            const SizedBox(height: 80),
             Padding(
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   FadeInUp(
-                      duration: Duration(milliseconds: 1000),
-                      child: Row(
-                        children: [
+                    duration: const Duration(milliseconds: 1000),
+                    child: Row(
+                      children: const [
                         Icon(
                           Icons.palette_outlined,
                           color: Colors.deepPurpleAccent,
                           size: 50,
                         ),
-                          Text(
-                            "Artify",
-                            style: TextStyle(color: Colors.white, fontSize: 50),
-                          )
-            ]),
-                      ),
-                  SizedBox(
-                    height: 40,
+                        Text(
+                          "Artify",
+                          style: TextStyle(color: Colors.white, fontSize: 50),
+                        )
+                      ],
+                    ),
                   ),
+                  const SizedBox(height: 40),
                   FadeInUp(
-                      duration: Duration(milliseconds: 1300),
-                      child: Text(
-                        "Modern Sanat Galerisi, Telefonunuzu Güzelleştirin",
-                        style: TextStyle(color: Colors.white, fontSize: 25),
-                      )),
+                    duration: const Duration(milliseconds: 1300),
+                    child: const Text(
+                      "Modern Sanat Galerisi, Telefonunuzu Güzelleştirin.",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ),
                 ],
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Expanded(
               child: Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 36, 36, 36),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60))),
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 36, 36, 36),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
+                ),
                 child: Padding(
-                  padding: EdgeInsets.all(30),
+                  padding: const EdgeInsets.all(30),
                   child: Column(
                     children: <Widget>[
-                      SizedBox(
-                        height: 60,
-                      ),
+                      const SizedBox(height: 60),
                       FadeInUp(
-                          duration: Duration(milliseconds: 1400),
-                          child: Container(
-                            decoration: BoxDecoration(
+                        duration: const Duration(milliseconds: 1400),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 36, 36, 36),
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: const [
+                              BoxShadow(
                                 color: Color.fromARGB(255, 36, 36, 36),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Color.fromARGB(255, 36, 36, 36),
-                                      blurRadius: 20,
-                                      offset: Offset(0, 10))
-                                ]),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Colors.white))),
-                                  child: TextField(
-                                    style: TextStyle(color: Colors.white),
-                                    decoration: InputDecoration(
-                                        hintText: "Kullanıcı Adı",
-                                        hintStyle:
-                                        TextStyle(color: Colors.white),
-                                        border: InputBorder.none),
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(color: Colors.white),
                                   ),
                                 ),
-                                Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Colors.white))),
-                                  child: TextField(
-                                    obscureText: true,
-                                    style: TextStyle(color: Colors.white),
-                                    decoration: InputDecoration(
-                                        hintText: "Şifre",
-                                        hintStyle:
-                                        TextStyle(color: Colors.white),
-                                        border: InputBorder.none),
+                                child: const TextField(
+                                  style: TextStyle(color: Colors.white),
+                                  decoration: InputDecoration(
+                                    hintText: "Kullanıcı Adı",
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    border: InputBorder.none,
                                   ),
                                 ),
-                              ],
-                            ),
-                          )),
-                      SizedBox(
-                        height: 40,
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(color: Colors.white),
+                                  ),
+                                ),
+                                child: TextField(
+                                  obscureText: !_isPasswordVisible,
+                                  style: const TextStyle(color: Colors.white),
+                                  decoration: InputDecoration(
+                                    hintText: "Şifre",
+                                    hintStyle: const TextStyle(color: Colors.white),
+                                    border: InputBorder.none,
+                                    suffixIcon: IconButton(
+                                      icon: Icon(
+                                        _isPasswordVisible
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          _isPasswordVisible = !_isPasswordVisible;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
+                      const SizedBox(height: 40),
                       FadeInUp(
-                          duration: Duration(milliseconds: 1600),
-                          child: MaterialButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
+                        duration: const Duration(milliseconds: 1600),
+                        child: MaterialButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
                               context,
                               MaterialPageRoute(builder: (context) => Main()),
-                              );
-                            },
-                            height: 50,
-                            color: Colors.deepPurpleAccent,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Giriş Yap",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                            );
+                          },
+                          height: 50,
+                          color: Colors.deepPurpleAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "Giriş Yap",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          )),
-                      SizedBox(
-                        height: 50,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      FadeInUp(
+                        duration: const Duration(milliseconds: 1500),
+                        child: const Text(
+                          "Hesabın mı yok? Hemen ",
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ),
                       FadeInUp(
-                          duration: Duration(milliseconds: 1500),
-                          child: Text(
-                            "Hesabın mı yok? Hemen ",
-                            style: TextStyle(color: Colors.grey),
-                          )),
-                      FadeInUp(
-                          duration: Duration(milliseconds: 1500),
-                          child: MaterialButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(builder: (context) => Register()),
-                              );
-                            },
-                            child: Center(
-                              child: Text(
-                                "Hesap Oluştur",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                        duration: const Duration(milliseconds: 1500),
+                        child: MaterialButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const Register()),
+                            );
+                          },
+                          child: const Center(
+                            child: Text(
+                              "Hesap Oluştur",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          )),
-                      SizedBox(
-                        height: 40,
+                          ),
+                        ),
                       ),
-                      SizedBox(
-                        height: 40,
-                      ),
+                      const SizedBox(height: 40),
                     ],
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
