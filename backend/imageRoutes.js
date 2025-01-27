41845b20423b210f0188d38576394cbe92c6d2bd
AksitArda <aksithuseyinarda@gmail.com>
2025-01-27 19:04:15 +0300

    Update imageRoutes.js

--- backend/imageRoutes.js
+++ backend/imageRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const multer = require("multer");
-const Data = require("./post/postModel");
+const { Data } = require("./post/postModel");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
